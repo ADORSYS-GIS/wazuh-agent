a212b2f2c7ee655bca5cb4d6779144bc1013b0ee
Stephane Segning Lambou <stephane.segning-lambou@adorsys.com>
2024-09-16 13:30:11 +0200

    fix: fixed install script

--- scripts/setup-agent.sh
+++ scripts/setup-agent.sh
@@ -3,7 +3,7 @@
 # Default log level and application details
 LOG_LEVEL=${LOG_LEVEL:-"INFO"}
 APP_NAME=${APP_NAME:-"wazuh-cert-oauth2-client"}
-WOPS_VERSION=${WOPS_VERSION:-"0.1.5"}
+WOPS_VERSION=${WOPS_VERSION:-"0.2.1"}
 OSSEC_CONF_PATH=${OSSEC_CONF_PATH:-"/var/ossec/etc/ossec.conf"}
 USER=${USER:-"root"}
 GROUP=${GROUP:-"wazuh"}
