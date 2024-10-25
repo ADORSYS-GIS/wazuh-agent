e5fb3ae602e13504c29b0d4a9a5440688a600f67
ArmandMeppa <meppaarmand@gmail.com>
2024-10-25 11:58:26 +0100

    refactor: improve install.ps1

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -37,61 +37,45 @@ function Install-Agent {
     # Global variables
     $OSSEC_CONF_PATH = "C:\Program Files (x86)\ossec-agent\ossec.conf"
 
-    # Function to install Wazuh agent
-    function Install-WazuhAgent {
-        # Variables
-        $WAZUH_MANAGER = "master.dev.wazuh.adorsys.team"
-        $WAZUH_AGENT_VERSION = "4.8.1-1"
-        $WAZUH_AGENT_MSI = "wazuh-agent-${WAZUH_AGENT_VERSION}.msi"
-        $TEMP_DIR = $env:TEMP
+    # Variables
+    $WazuhManager = "master.dev.wazuh.adorsys.team"
+    $AgentVersion = "4.8.1-1"
+    $AgentFileName = "wazuh-agent-$AgentVersion.msi"
+    $TempDir = $env:TEMP
+    $DownloadUrl = "https://packages.wazuh.com/4.x/windows/wazuh-agent-$AgentVersion.msi"
+    $MsiPath = Join-Path -Path $TempDir -ChildPath $AgentFileName
 
-        # Determine package URL based on architecture
-        if ([System.Environment]::Is64BitOperatingSystem) {
-            $PACKAGE_URL = "https://packages.wazuh.com/4.x/windows/wazuh-agent-${WAZUH_AGENT_VERSION}.msi"
-        } else {
-            error_message "Unsupported architecture. Only 64-bit systems are supported."
-            exit 1
-        }
-
-        # Download the package
-        info_message "Downloading Wazuh agent..."
-        $msiPath = Join-Path -Path $TEMP_DIR -ChildPath $WAZUH_AGENT_MSI
-        try {
-            Invoke-WebRequest -Uri $PACKAGE_URL -OutFile $msiPath -ErrorAction Stop
-        } catch {
-            error_message "Failed to download Wazuh agent: $_"
-            exit 1
-        }
-
-        # Install the package
-        info_message "Installing Wazuh agent..."
-        $MSIArguments = @(
-            "/i"
-            "`"$msiPath`""   # Use backticks to escape the quotes in the path
-            "/quiet"
-            "/norestart"  # Prevent restart after installation
-            "WAZUH_MANAGER=${WAZUH_MANAGER}"
-        )
-
-        try {
-            Start-Process -FilePath "msiexec.exe" -ArgumentList $MSIArguments -Wait -ErrorAction Stop
-        } catch {
-            error_message "Failed to install Wazuh agent: $_"
-            exit 1
-        }
-
-        # Clean up
-        info_message "Cleaning up..."
-        try {
-            Remove-Item -Path $msiPath -ErrorAction Stop
-        } catch {
-            Write-Warning "Failed to clean up the downloaded MSI file: $_"
-        }
-        info_message "Wazuh agent installed successfully!"
+    # Check if system architecture is supported
+    if (-not [System.Environment]::Is64BitOperatingSystem) {
+        error_message "Unsupported architecture. Only 64-bit systems are supported."
+        return
     }
 
-    # Call the Install-WazuhAgent function
-    Install-WazuhAgent
+    # Download the Wazuh agent MSI package
+    info_message "Downloading Wazuh agent version $AgentVersion..."
+    try {
+        Invoke-WebRequest -Uri $DownloadUrl -OutFile $MsiPath -ErrorAction Stop
+    } catch {
+        error_message "Failed to download Wazuh agent: $($_.Exception.Message)"
+        return
+    }
+
+    # Define MSI installation arguments
+    $msiArguments = @(
+        "/i", "`"$MsiPath`"", "/quiet", "/norestart",
+        "WAZUH_MANAGER=$WazuhManager"
+    )
+
+    # Install the Wazuh agent
+    info_message "Installing Wazuh agent..."
+    try {
+        Start-Process -FilePath "msiexec.exe" -ArgumentList $msiArguments -Wait -ErrorAction Stop
+    } catch {
+        error_message "Failed to install Wazuh agent: $($_.Exception.Message)"
+        return
+    }
+
+    info_message "Wazuh agent installed successfully."
 }
 
 # Call the Install-Agent function to execute the installation
