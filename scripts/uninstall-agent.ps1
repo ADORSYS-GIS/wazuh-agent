c31fc03ead5d9fc50b76ef9d4d0226c515c2636a
MarantosGeorge <marantosgeorge@gmail.com>
2025-01-29 11:57:37 +0200

    chore:update url's for uninstall scripts

--- scripts/uninstall-agent.ps1
+++ scripts/uninstall-agent.ps1
@@ -4,7 +4,6 @@ Set-StrictMode -Version Latest
 # Variables (default log level, app details, paths)
 $LOG_LEVEL = if ($env:LOG_LEVEL) { $env:LOG_LEVEL } else { "INFO" }
 $OSSEC_CONF_PATH = "C:\Program Files (x86)\ossec-agent\ossec.conf" # Adjust for Windows
-$TEMP_DIR = [System.IO.Path]::GetTempPath()
 
 # Function to log messages with a timestamp
 function Log {
@@ -42,7 +41,7 @@ function Uninstall-WazuhAgent {
     try {
         Write-Host "Downloading and executing Wazuh agent uninstall script..."
 
-        $UninstallerURL = ""  # Update the URL if needed
+        $UninstallerURL = "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-agent/refs/heads/uninstall-script/issue-25/scripts/uninstall.ps1"  # Update the URL if needed
         $UninstallerPath = "$env:TEMP\uninstall.ps1"
 
         # Download Wazuh agent Uninstaller script
@@ -65,41 +64,13 @@ function Uninstall-WazuhAgent {
 }
 
 
-# Step 2: Download and Uninstall wazuh-cert-oauth2-client with error handling
-function Uninstall-OAuth2Client {
-    try {
-        Write-Host "Downloading and executing wazuh-cert-oauth2-client uninstall script..."
-
-        $OAuth2Url = ""  # Update the URL if needed
-        $OAuth2Script = "$env:TEMP\uninstall-wazuh-cert-oauth2-client.ps1"
-
-        # Download the wazuh-cert-oauth2-client Uninstaller script
-        Invoke-WebRequest -Uri $OAuth2Url -OutFile $OAuth2Script -ErrorAction Stop
-        Write-Host "wazuh-cert-oauth2-client script downloaded successfully."
-
-        #Supposed to remove the execution/ compare to .sh version
-        # Execute the downloaded script with required parameters
-        & powershell.exe -ExecutionPolicy Bypass -File $OAuth2Script -ArgumentList "-LOG_LEVEL", $LOG_LEVEL, "-OSSEC_CONF_PATH", $OSSEC_CONF_PATH, "-APP_NAME", $APP_NAME, "-WOPS_VERSION", $WOPS_VERSION -ErrorAction Stop
-    }
-    catch {
-        Write-Host "Error during wazuh-cert-oauth2-client Uninstallation: $($_.Exception.Message)" -ForegroundColor Red
-    }
-    finally {
-        # Clean up the Uninstaller file if it exists
-        if (Test-Path $OAuth2Script) {
-            Remove-Item $OAuth2Script -Force
-            Write-Host "Uninstall file removed."
-        }
-    }
-}
-
 
 # Step 3: Download and Uninstall YARA with error handling
 function Uninstall-Yara {
     try {
         Write-Host "Downloading and executing YARA uninstall script..."
 
-        $YaraUrl = ""  # Update the URL if needed
+        $YaraUrl = "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-yara/refs/heads/uninstall-script/issue-25/scripts/uninstall.ps1"  # Update the URL if needed
         $YaraScript = "$env:TEMP\uninstall-yara.ps1"
 
         # Download the Uninstallation script
@@ -126,7 +97,7 @@ function Uninstall-Snort {
     try {
         Write-Host "Downloading and executing Snort uninstall script..."
 
-        $SnortUrl = ""  # Update the URL if needed
+        $SnortUrl = "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-snort/refs/heads/uninstall-script/issue-25/scripts/windows/uninstall.ps1"  # Update the URL if needed
         $SnortScript = "$env:TEMP\uninstall-snort.ps1"
 
         # Download the Uninstallation script
@@ -153,7 +124,7 @@ function Uninstall-AgentStatus {
     try {
         Write-Host "Downloading and executing Wazuh Agent Status uninstall script..."
 
-        $AgentStatusUrl = ""  # Update the URL if needed
+        $AgentStatusUrl = "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-agent-status/refs/heads/uninstall-script/issue-25/scripts/uninstall.ps1"  # Update the URL if needed
         $AgentStatusScript = "$env:TEMP\uninstall-agent-status.ps1"
 
         # Download the Uninstallation script
@@ -181,8 +152,8 @@ function Uninstall-AgentStatus {
 # Main Execution
 SectionSeparator "Uninstalling Wazuh Agent"
 Uninstall-WazuhAgent
-SectionSeparator "Uninstalling OAuth2Client"
-Uninstall-OAuth2Client
+# SectionSeparator "Uninstalling OAuth2Client"
+# Uninstall-OAuth2Client
 SectionSeparator "Uninstalling Agent Status"
 Uninstall-AgentStatus
 SectionSeparator "Uninstalling Yara"
