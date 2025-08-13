82fb68cd58d222694ebbf7250ed70763a4464302
MarantosGeorge <marantosgeorge@gmail.com>
2025-08-13 12:29:17 +0100

    test: update Suricata script URL to test update to suricata script

--- scripts/setup-agent.sh
+++ scripts/setup-agent.sh
@@ -244,7 +244,7 @@ fi
 if [ "$IDS_ENGINE" = "suricata" ]; then
     uninstall_snort
     info_message "Installing Suricata in ${BOLD}${SURICATA_MODE}${NORMAL} mode..."
-    curl -sL --progress-bar "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-suricata/v$WAZUH_SURICATA_VERSION/scripts/install.sh" > "$TMP_FOLDER/install-suricata.sh"
+    curl -sL --progress-bar "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-suricata/refs/heads/fix/macos-brew-owner-detection/scripts/install.sh" > "$TMP_FOLDER/install-suricata.sh"
     # Pass the selected mode to the suricata install script
     if ! (bash "$TMP_FOLDER/install-suricata.sh" --mode "$SURICATA_MODE") 2>&1; then
         error_message "Failed to install 'suricata'"
