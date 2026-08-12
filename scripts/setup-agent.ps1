354620e2cd3439ff6ba769e9e608edd0206862aa
Caleb Asah <150007254+Calebasah@users.noreply.github.com>
2026-08-12 09:10:55 +0100

    chore: downgrade Wazuh agent version to 1.8.1-rc.1 and update checksums (#222)
    Co-authored-by: chojuninengu <chojuninengu@gmail.com>

--- scripts/setup-agent.ps1
+++ scripts/setup-agent.ps1
@@ -6,7 +6,7 @@ param(
 
 # Repository ref
 if (-not $Ref) {
-    if (-not $RepoVersion) { $RepoVersion = '1.9.0-rc.5' }
+    if (-not $RepoVersion) { $RepoVersion = '1.8.1-rc.1' }
     $Ref = "refs/tags/v${RepoVersion}"
 }
 
--- scripts/setup-agent.sh
+++ scripts/setup-agent.sh
@@ -7,7 +7,7 @@ set -eu
 # downloaded to a temporary location and is executed remotely.
 
 REPO_URL="https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-agent"
-REPO_VERSION=${WAZUH_AGENT_REPO_VERSION:-'1.9.0-rc.5'}
+REPO_VERSION=${WAZUH_AGENT_REPO_VERSION:-'1.8.1-rc.1'}
 REF=${WAZUH_AGENT_REPO_REF:-"refs/tags/v${REPO_VERSION}"}
 
 SCRIPT_NAME="setup-agent.sh"
