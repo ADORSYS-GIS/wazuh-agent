5b2b4fe4d37f2bffb4b28d46a213003eae8f89fd
chojuninengu <chojuninengu@gmail.com>
2026-09-17 17:00:35 +0100

    fix: correct OS-specific paths for YARA and Suricata uninstall scripts

--- scripts/windows/uninstall-agent.ps1
+++ scripts/windows/uninstall-agent.ps1
@@ -124,7 +124,7 @@ function Uninstall-AgentStatus {
 
 # Step 2: Download and Uninstall YARA with error handling
 function Uninstall-Yara {
-    $YaraUrl = "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-yara/$WAZUH_YARA_REPO_REF/scripts/uninstall.ps1"
+    $YaraUrl = "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-yara/$WAZUH_YARA_REPO_REF/scripts/windows/uninstall.ps1"
     $YaraScript = "$env:TEMP\uninstall-yara.ps1"
     $global:UninstallerFiles += $YaraScript
     try {
@@ -152,7 +152,7 @@ function Uninstall-Snort {
 
 # Step 4: Download and Uninstall Suricata with error handling
 function Uninstall-Suricata {
-    $SuricataUrl = "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-suricata/$WAZUH_SURICATA_REPO_REF/scripts/uninstall.ps1"
+    $SuricataUrl = "https://raw.githubusercontent.com/ADORSYS-GIS/wazuh-suricata/$WAZUH_SURICATA_REPO_REF/scripts/windows/uninstall.ps1"
     $SuricataScript = "$env:TEMP\uninstall-suricata.ps1"
     $global:UninstallerFiles += $SuricataScript
     try {
